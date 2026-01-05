d148c315b86c7a80d363714c6c2d8dade79676b0
Delight <delightgeorge105@gmail.com>
2026-01-05 10:13:52 +0100

    created index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 10;
+let sum = a + b;
+console.log("The sum of a and b is: " + sum);
+console.log("Hello, World!");
+console.log("This is a simple JavaScript program.");
+console.log("Goodbye, World!");
+console.log("End of the program.");
